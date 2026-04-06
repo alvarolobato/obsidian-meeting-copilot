de3143e96856b6757d8eb128b2e4d36d711adcfc
Yuto Taakgi <yutotkg.1040@gmail.com>
2026-04-06 17:48:25 +0900

    feat: add recording folder and file name template settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,36 +1,54 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
-import MyPlugin from "./main";
+import { App, PluginSettingTab, Setting } from "obsidian";
+import SystemRecordingPlugin from "./main";
 
-export interface MyPluginSettings {
-	mySetting: string;
+export interface SystemRecordingSettings {
+    recordingFolder: string;
+    fileNameTemplate: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export const DEFAULT_SETTINGS: SystemRecordingSettings = {
+    recordingFolder: "recordings",
+    fileNameTemplate: "recording-YYYY-MM-DD-HHmm",
+};
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class SystemRecordingSettingTab extends PluginSettingTab {
+    plugin: SystemRecordingPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: SystemRecordingPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    display(): void {
+        const { containerEl } = this;
+        containerEl.empty();
 
-		containerEl.empty();
+        new Setting(containerEl)
+            .setName("Recording folder")
+            .setDesc("Folder in your vault to save recordings")
+            .addText((text) =>
+                text
+                    .setPlaceholder("recordings")
+                    .setValue(this.plugin.settings.recordingFolder)
+                    .onChange(async (value) => {
+                        this.plugin.settings.recordingFolder = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
 
-		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName("File name template")
+            .setDesc(
+                "Template for recording file names. YYYY, MM, DD, HH, mm are replaced with date/time."
+            )
+            .addText((text) =>
+                text
+                    .setPlaceholder("recording-YYYY-MM-DD-HHmm")
+                    .setValue(this.plugin.settings.fileNameTemplate)
+                    .onChange(async (value) => {
+                        this.plugin.settings.fileNameTemplate = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
+    }
 }
